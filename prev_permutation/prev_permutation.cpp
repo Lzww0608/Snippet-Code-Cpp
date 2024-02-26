2e4855b8011de83560f78c8fbce13f6b2d667f22
Lzww <63537700+Lzww0608@users.noreply.github.com>
2024-02-26 18:33:01 +0800

    Add files via upload

---
+++ prev_permutation/prev_permutation.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+#include <vector>
+//输出从 7 个不同元素中取出 3 个元素的所有组合。
+int main() {
+	int values[] = { 1,2,3,4,5,6,7 };
+	int elements[] = { 1,1,1,0,0,0,0 };
+	const size_t N = sizeof(elements) / sizeof(elements[0]);
+	assert(N == sizeof(values) / sizeof(values[0]));
+	std::vector<int>selectors(elements, elements + N);
+
+	int count = 0;
+	do {
+		std::cout << ++count << ": ";
+		for (size_t i = 0; i < selectors.size(); ++i) {
+			if (selectors[i] == 1) {
+				std::cout << values[i] << "，";
+			}
+		}
+		std::cout << std::endl;
+	} while (prev_permutation(selectors.begin(), selectors.end()));
+	return 0;
+}
